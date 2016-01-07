62205a7609960bac2682a0a58251891fa99c4f7b
abuabdul <abuabdul86@hotmail.com>
2016-01-07 23:37:53 +0530

    adding test

--- src/main/java/com/abuabdul/practice/mockito/application/ecommerce/flipkart/FlipkartEcommerce.java
+++ src/main/java/com/abuabdul/practice/mockito/application/ecommerce/flipkart/FlipkartEcommerce.java
@@ -12,50 +12,50 @@ import com.abuabdul.practice.mockito.application.ecommerce.OnlineEcommerce;
 
 public class FlipkartEcommerce implements OnlineEcommerce, ApplicationRunConfig {
 
-    private final static Logger logger = Logger.getLogger(FlipkartEcommerce.class.getName());
+	private final static Logger logger = Logger.getLogger(FlipkartEcommerce.class.getName());
 
-    public void showCatalogOfProducts() {
+	public void showCatalogOfProducts() {
+		System.out.println("Showing flipkart");
+	}
 
-    }
+	public void viewCartPage() {
 
-    public void viewCartPage() {
+	}
 
-    }
+	public void displayWishlistProducts() {
 
-    public void displayWishlistProducts() {
+	}
 
-    }
+	public void purchaseProducts() {
 
-    public void purchaseProducts() {
+	}
 
-    }
-
-
-    public void run() {
-        logger.info("1. Show Products Catalog");
-        logger.info("2. Go to Cart page");
-        logger.info("3. Display Wishlist");
-        logger.info("4. Exit application");
-        Scanner scan = new Scanner(System.in);
-        int options = scan.nextInt();
-        do{
-            switch(options){
-                case 1:
-                    logger.info("Show catalogs");
-                    showCatalogOfProducts();
-                    break;
-                case 2:
-                    logger.info("Go to Cart page");
-                    viewCartPage();
-                    break;
-                case 3:
-                    logger.info("View Wishlist");
-                    displayWishlistProducts();
-                    break;
-                case 4: break;
-            }
-            options = scan.nextInt();
-        } while(options!=4);
-    }
+	public void run() {
+		logger.info("1. Show Products Catalog");
+		logger.info("2. Go to Cart page");
+		logger.info("3. Display Wishlist");
+		logger.info("4. Exit application");
+		Scanner scan = new Scanner(System.in);
+		int options = scan.nextInt();
+		do {
+			switch (options) {
+			case 1:
+				logger.info("Show catalogs");
+				showCatalogOfProducts();
+				break;
+			case 2:
+				logger.info("Go to Cart page");
+				viewCartPage();
+				break;
+			case 3:
+				logger.info("View Wishlist");
+				displayWishlistProducts();
+				break;
+			case 4:
+				break;
+			}
+			options = scan.nextInt();
+		} while (options != 4);
+	}
 
 }
